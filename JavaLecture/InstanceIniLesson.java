78e2f3c7b3847038b1ba3ca7031d4652d8078e7f
AnnaMariceBoyose-Dev <129864461+devAMBoyose@users.noreply.github.com>
2025-10-23 09:32:23 +0000

    example study

--- JavaLecture/InstanceIniLesson.java
+++ JavaLecture/InstanceIniLesson.java
@@ -1,9 +1,11 @@
 package chapter1;
 
 public class InstanceIniLesson {
+    int code;
     public static void main(String[] args) {
         InstanceIniLesson ini = new InstanceIniLesson();
         System.out.println("feathers");
     }
-   {System.out.println(x:"snowy");} 
+   {System.out.println(x:"snowy");} // instance initializer block
+   code
 }
