d137f18ec717ad59ece96512d22258b5dd168c0b
AnnaMariceBoyose-Dev <129864461+devAMBoyose@users.noreply.github.com>
2025-10-24 17:26:47 +0000

    void setprice

--- JavaLecture/Day9/Main.java
+++ JavaLecture/Day9/Main.java
@@ -9,19 +9,19 @@
          */
 
 public class Main {
-
     public static void main(String[] args) {
-
         Toy toy1 = new Toy();
         toy1.name = "Range pink";
         toy1.brand = "ToyBrand";
         toy1.price = 10.99;
         toy1.quantity = 5;
 
+        toy1.setPrice(toy1.price * 0.5); // divide by 2
+
         System.out.println("Toy Name: " + toy1.name);
         System.out.println("Toy Brand: " + toy1.brand);
         System.out.println("Toy Price: $" + toy1.price);
         System.out.println("Toy Quantity: " + toy1.quantity);
     }
-
 }
+
--- JavaLecture/Day9/Toy.java
+++ JavaLecture/Day9/Toy.java
@@ -1,16 +1,14 @@
 public class Toy {
     String name;
     String brand;
-    double price; // Attribute to hold the price of the toy
+    double price;
     int quantity;
 
-    void setPrice(double price) { // Method to set the price if changed
-        if (this.price != price) {
-            this.price = price; // 'this' refers to the current object's price
-        }
+    void setPrice(double price) {
+        this.price = price;  // updates price
     }
-
-    public static void main(String[] args) {
+    
+        public static void main(String[] args) {
         Toy toy1 = new Toy();
         toy1.name = "Range pink";
         toy1.brand = "ToyBrand";
@@ -19,18 +17,17 @@ public class Toy {
         toy1.setPrice((toy1.price*0.5)); // Using the setPrice method to update the price detail divided by 2
 
     }
-
-    class InnerToy1 {   
-        
-    }
-
-    class InnerToy2 {   
-        
-    }
-
-    class InnerToy3 {   
-        
-    } //multiple inner classes one public class only
+    //multiple inner classes one public class only
 }
 
-//if you have multiple classes only one class can be public and the file name should be the same as the public class name
+class InnerToy1 {
+
+}
+
+class InnerToy2 {
+
+}
+
+class InnerToy3 {
+
+}
