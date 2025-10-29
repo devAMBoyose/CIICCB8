142d0ffb05277464b56ba43891466bfe8b4c49da
AnnaMariceBoyose-Dev <129864461+devAMBoyose@users.noreply.github.com>
2025-10-29 08:29:47 +0000

    task3 print Success!

---
+++ Quiz/task3/Task3.java
@@ -0,0 +1,20 @@
+public class Task3 {
+    public static void main(String[] args) {
+
+        // (Strings)
+        String a = "Wow";      
+        String b = a;
+        String c = "Hello";
+        String d = "Wowr";
+
+        // conditions
+        boolean b1 = (a == b);             
+        boolean b2 = d.equals(b + "r");
+        boolean b3 = !c.equals(a);
+
+        // print "Success!"
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+    }
+}
