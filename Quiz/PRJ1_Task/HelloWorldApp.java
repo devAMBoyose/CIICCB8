4d8274883c4af74e81c338c0af59f865a1a68cdb
AnnaMariceBoyose-Dev <129864461+devAMBoyose@users.noreply.github.com>
2025-10-30 10:16:39 +0000

    PRJ_Task1-1-1

---
+++ Quiz/PRJ1_Task/HelloWorldApp.java
@@ -0,0 +1,21 @@
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+// clas helloWorldApp {
+//     public static void main(String[] args) {
+//         System.out.rintln("Hello World!); // Display the string.
+//     }
+// } 
+
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!");
+    }
+}
+
+// The code has errors because clas is misspelled, it should be class. The class
+// name helloWorldApp must start with a capital letter and match the file name
+// HelloWorldApp. The print command System.out.rintln is missing the letter p,
+// and the text "Hello World! is missing a closing quotation mark ("). Fixing
+// these errors makes the program run and show “Kamusta Mundo!” correctly.
