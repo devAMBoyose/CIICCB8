fc9685c0bfc3d0c6e31fcc67067b8b93080e18e7
AnnaMariceBoyose-Dev <129864461+devAMBoyose@users.noreply.github.com>
2025-10-30 09:28:47 +0000

    Chapter2_Part1_Task3

--- Quiz/task3/Task3.java
+++ Quiz/task3/Task3.java
@@ -1,24 +1,20 @@
 public class Task3 {
     public static void main(String[] args) {
 
-        // String a = new String("Wow");
-        // String b = "Wow";
-        // String c = a;
-        // String d = c;
-
         // (Strings)
-        String a = "Wow";      
-        String b = a;
-        String c = "Hello";
-        String d = "Wow!"; // change from "Wowr" to "Wow!"
+        String a = "Anna"; 
+        String b = a; 
+        String c = "Boyose"; 
+        String d = new String("Anna") + "!"; 
 
-        boolean b1 = (a == b);             
-        boolean b2 = d.equals(b + "!");
-        boolean b3 = !c.equals(a);
+        // (Booleans)
+        boolean b1 = (a == b); 
+        boolean b2 = d.equals(b + "!"); 
+        boolean b3 = !c.equals(a); 
 
+        // (Condition)
         if (b1 && b2 && b3) {
             System.out.println("Success!");
         }
-
     }
 }
