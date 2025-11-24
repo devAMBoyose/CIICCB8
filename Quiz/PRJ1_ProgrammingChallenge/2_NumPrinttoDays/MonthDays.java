2f1eaf4c5a619b51e6297b73a049dc9aae86cbc5
AnnaMariceBoyose-Dev <129864461+devAMBoyose@users.noreply.github.com>
2025-11-24 07:33:33 +0000

    PRJ_ProgrammingChallenge_MonthDays

---
+++ Quiz/PRJ1_ProgrammingChallenge/2_NumPrinttoDays/MonthDays.java
@@ -0,0 +1,45 @@
+public class MonthDays {
+    public static void main(String[] args) {
+
+        for (int month = 1; month <= 12; month++) {
+            switch (month) {
+                case 1:
+                    System.out.println("1 - January has 31 days.");
+                    break;
+                case 2:
+                    System.out.println("2 - February has 28 days.");
+                    break;
+                case 3:
+                    System.out.println("3 - March has 31 days.");
+                    break;
+                case 4:
+                    System.out.println("4 - April has 30 days.");
+                    break;
+                case 5:
+                    System.out.println("5 - May has 31 days.");
+                    break;
+                case 6:
+                    System.out.println("6 - June has 30 days.");
+                    break;
+                case 7:
+                    System.out.println("7 - July has 31 days.");
+                    break;
+                case 8:
+                    System.out.println("8 - August has 31 days.");
+                    break;
+                case 9:
+                    System.out.println("9 - September has 30 days.");
+                    break;
+                case 10:
+                    System.out.println("10 - October has 31 days.");
+                    break;
+                case 11:
+                    System.out.println("11 - November has 30 days.");
+                    break;
+                case 12:
+                    System.out.println("12 - December has 31 days.");
+                    break;
+            }
+        }
+    }
+}
