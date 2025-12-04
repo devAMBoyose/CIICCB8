68e3823231ed14d580ebd85a86d35b1c9864f7be
AnnaMariceBoyose-Dev <129864461+devAMBoyose@users.noreply.github.com>
2025-12-04 19:24:34 +0000

    task7 multiplication

---
+++ Quiz/OnlineClass/Chapter4/Task7.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Task7 {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+
+        System.out.println("Addition: " + add(num1, num2));
+        System.out.println("Subtraction: " + subtract(num1, num2));
+        System.out.println("Multiplication: " + multiply(num1, num2));
+        System.out.println("Division: " + divide(num1, num2));
+    }
+}
